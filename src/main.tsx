720cd695cb329d5ee4df4a767028a537778f6f7f
Anton Vynnyk <anton.vynnyk.8118@gmail.com>
2023-09-27 21:29:26 +0300

    feat(root): create base structure

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "@/styles/index.css";
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
