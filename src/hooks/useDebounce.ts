affe50eba03765f7d4b432ead826211520ea205a
Anton Vynnyk <anton.vynnyk.8118@gmail.com>
2023-10-10 10:21:49 +0300

    feat(T08): create changing theme

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 
-export function useDebounce<T>(value: T, delay?: number): T {
+const DEFAULT_DELAY = 500;
+
+export function useDebounce<T>(value: T, delay = DEFAULT_DELAY): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay ?? 500);
+    const timer = setTimeout(() => setDebouncedValue(value), delay);
 
     return () => {
       clearTimeout(timer);
     };
-  }, [value, delay]);
+  }, [value]);
 
   return debouncedValue;
 }
