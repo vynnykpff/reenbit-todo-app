1d0f4840db4281e10fc1b993ec8d8d370d1c24f9
Anton Vynnyk <anton.vynnyk.8118@gmail.com>
2023-12-27 10:00:27 +0200

    fix(N04-client): replace auth api path

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from "axios";
 import { DEFAULT_ERROR_MESSAGE } from "@/common/constants/ErrorMessages.ts";
 
 export const authApi = axios.create({
-  baseURL: "http://localhost:5000/api/users",
+  baseURL: "https://reenbit-todo-app.onrender.com/api/users",
 });
 
 authApi.interceptors.response.use(
