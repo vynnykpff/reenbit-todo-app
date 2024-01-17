d389ae10df522906f0495b97587e6128d5c5265e
Anton Vynnyk <anton.vynnyk.8118@gmail.com>
2024-01-17 15:58:22 +0200

    Updates

--- assets/NotFoundPage-ac8e2927.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-ce50c5d6.js";const e=()=>o.jsx("h2",{children:"Not Found Page: 404"});export{e as default};
---
+++ assets/NotFoundPage-b6cf317a.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-cb00505e.js";const e=()=>o.jsx("h2",{children:"Not Found Page: 404"});export{e as default};
